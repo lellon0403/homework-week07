d3623ebe7746d76318dd6e0696667f2db3e276d2
lellon0403 <a67733198@gmail.com>
2025-10-19 18:36:15 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -54,5 +54,33 @@ class _Time extends StatelessWidget{
   const _Time({
     required this.startTime;
     required this.endTime;
-  })
+    Key? key
+  }) : super (key: key);
+
+  @override
+  Widget build(BuildContext context){
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: PRIMARY_COLOR,
+      fontSize: 16.0,
+    );
+
+    return Column( //시간을 위에서 아래로 배치
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          //숫자가 두 자릿수가 안되면 0 으로 채워주기
+          '${startTime.toString().padLeft(2,'0')}:00',
+          style: textStyle,
+        ),
+        Text(
+          //숫자가 두 자릿수가 안 되면 0으로 채워주기
+          '${startTime.toString().padLeft(2,'0')}:00',
+          style: textStyle.copyWith(
+            fontSize: 10.0,
+          ),
+        )
+      ],
+    );
+  }
 }
