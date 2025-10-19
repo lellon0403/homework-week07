bd792d2c819d72bdcdbfb649b812ab1da88ebf02
lellon0403 <a67733198@gmail.com>
2025-10-19 16:30:14 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -3,5 +3,7 @@ import 'package:drift/drift.dart';
 class Schedules extends Table{
   IntColumn get id => integer().autoIncrement()(); // PRIMARY KEY, 정수 열
   TextColumn get content => text()(); // 내용, 글자 열
-  
+  DateTimeColumn get date => dateTime()(); //일정 날짜, 날짜 열
+  IntColumn startTime => integer()(); //시작 시간
+  IntColumn get endTime => integer()();
 }
