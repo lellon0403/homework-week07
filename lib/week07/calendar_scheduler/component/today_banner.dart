6cf284b977d8fd70813cf9ed65bd9f2a436badfe
lellon0403 <a67733198@gmail.com>
2025-10-19 18:29:15 +0900

    Updated Changes

---
+++ lib/week07/calendar_scheduler/component/today_banner.dart
@@ -0,0 +1,23 @@
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class TodayBanner extends StatelessWidget{
+  final DateTime selectedDate; //선택된 날짜
+  final int count; 일정 개수
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle = TextStyle( //기본으로 사용할 글꼴
+      FontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+
+    return Container
+  }
+}
