caabbbe3e8ed05d40965c7e127301ced6153b51c
lellon0403 <a67733198@gmail.com>
2025-10-19 16:41:13 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -4,6 +4,6 @@ class Schedules extends Table{
   IntColumn get id => integer().autoIncrement()(); // PRIMARY KEY, 정수 열
   TextColumn get content => text()(); // 내용, 글자 열
   DateTimeColumn get date => dateTime()(); //일정 날짜, 날짜 열
-  IntColumn startTime => integer()(); //시작 시간
+  IntColumn get startTime => integer()(); //시작 시간
   IntColumn get endTime => integer()();
 }
