9dd18b390050ff7ff87f7d5e2e6ef534b8bd7913
lellon0403 <a67733198@gmail.com>
2025-10-19 17:00:44 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -15,4 +15,32 @@ tables: [
 ],
 )
 
-class LocalDatabase extends _$LocalDatabase {}
+class LocalDatabase extends _$LocalDatabase {
+
+  LocalDatabase(): super(_openConnection());
+
+  Stream<List<Schedule>> watchSchedules(DateTime date) =>
+  //데이터를 조회하고 변화 감지
+  (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+  //watch 함수 아래에 작성
+  Future<int> createSchedule(SchedulesCompanion date) =>
+  into(schedules).insert(data);
+
+  Future<int> removeSchedule(int id) =>
+  (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+  
+  //removeSchedule() 함수 아래에 작성
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+
+    //데이터베이스 파일 저장할 폴더
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path,'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+//Code Generation으로 생성할 클래스 상속
