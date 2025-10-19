bf844f434bdad29d2080b15bc64d6883c268aeec
lellon0403 <a67733198@gmail.com>
2025-10-19 18:18:45 +0900

    Updated Changes

---
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -0,0 +1,8 @@
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ScheduleCard extends StatelessWidget{ 
+  final int startTime;
+  final int endTime;
+  fin
+}
