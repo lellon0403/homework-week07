1ba76cce18d2223fe8f3836c721a5d85a8f2726a
lellon0403 <a67733198@gmail.com>
2025-10-19 17:35:44 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/custom_text_field.dart
+++ lib/week07/calendar_scheduler/component/custom_text_field.dart
@@ -29,7 +29,32 @@ class CustomTextField extends StatelessWidget{
           ),
         ),
 
-
+        Expanded(
+          flex: isTime? 0 : 1, //
+          child: TextFormField(
+            onSaved: onSaved,  //폼 저장했을 때 실행할 함수
+            validator: validator, //폼 검증했을 때 실행할 함수
+            cursorColor: Colors.grey, //커서 색상 변경
+            maxLength: isTime ? 1 : null,
+            //시간 관련 텍스트 필드가 아니면 한 줄 이상 작성 가능
+            expands: !isTime, //시간 관련 텍스트 필드는 공간 최대 차지
+            keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+            //시간 관련 텍스트 필드는 기본 숫자 키보드 아니면 일반 글자 키보드 보여주기
+            inputFormatters: isTime
+            ?[
+              FilteringTextInputFormatter.digitsOnly,
+            ]
+            : [], //시간 관련 텍스트 필드는 숫자만 입력하도록 제한
+            decoration: InputDecoration(
+              border: InputBorder.none, //테두리 삭제
+              filled: true, //배경색을 지정하겠다는 선언
+              fillColor: Colors.grey[300], //배경색
+              suffixText: isTime ? '시' : null,
+              //시간 관련 텍스트 필드는 '시' 접미사 추기            ),
+          ),
+        ),
+        ),
+        TextFormField(), //폼 안에서 텍스트 필드를 쓸 때 사용
       ],
     );
   }
