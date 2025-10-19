e1aed4fbb846f210e156d58f4cd716466ba5454e
lellon0403 <a67733198@gmail.com>
2025-10-19 18:26:15 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -26,6 +26,20 @@ class ScheduleCard extends StatelessWidget{
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: IntrinsicHeight( // 높이를 내부 위젯들의 최대 높이로 설정
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(// 시작과 종료 시간을 보여줄 위젯
+                startTime: startTime,
+                endTime: endTime,
+              ),
+              SizedBox(width: 16.0,),
+              _Content( //일정 내용을 보여줄 위젯
+                context: content,
+              ),
+              SizedBox(width: 16.0,),
+            ],
+          ),
         ),
       ),
       
