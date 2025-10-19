f3e9ae0ea3526ed986cd28875789980b5fe96f34
lellon0403 <a67733198@gmail.com>
2025-10-19 18:21:45 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/schedule_card.dart
+++ lib/week07/calendar_scheduler/component/schedule_card.dart
@@ -4,5 +4,31 @@ import 'package:flutter/material.dart';
 class ScheduleCard extends StatelessWidget{ 
   final int startTime;
   final int endTime;
-  fin
+  final int content;
+
+  const ScheduleCard({  
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {  
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IntrinsicHeight( 높이를 내부 위젯들의 최대 높이로 설정
+        ),
+      ),
+      
+    );
+  }
 }
