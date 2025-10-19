44338914c840854846d72bf773ac436552609eff
lellon0403 <a67733198@gmail.com>
2025-10-19 17:40:14 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/component/main_calendar.dart
+++ lib/week07/calendar_scheduler/component/main_calendar.dart
@@ -16,7 +16,15 @@ class MainCalendar extends StatelessWidget{
     return TableCalendar(
       locale: 'ko_kr', //한국어로 언어 변경
       onDaySelected: onDaySelected,
-      selectedDayPredicate: ,
-    )
+      selectedDayPredicate: (date) => //선택된 날짜를 구분할 로직
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
+        date.day == selectedDate.day,
+
+        firstDay: DateTime(1800,1,1), //첫째 날
+        lastDay: DateTime(3000,1,1), //마지막 날
+        focusedDay: DateTime.now(), //화면에 보여지느날
+        
+    );
   }
 }
